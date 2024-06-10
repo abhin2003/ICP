d47abe53a66fd7d853d0274097cf1bfcb7b33303
Abhin S Krishna <unniskrishna3@gmail.com>
2024-06-10 21:31:15 +0530

    task 5 first part

---
+++ src/first_project_frontend/src/AddMember.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function AddMember({ handleAddMember, name, setName, age, setAge }) {
+  return (
+    <div>
+      <h2>Add Member</h2>
+      <form onSubmit={handleAddMember}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="age">Age:</label>
+          <input
+            type="number"
+            id="age"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Add Member</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddMember;
